2668e3ab16dab307b0a474d7032c2b4ca98bfef4
Ryan Weiner <79421575+ryan-jordanweiner@users.noreply.github.com>
2023-02-22 01:20:43 -0500

    Create encryption.h
    header file for encryption.c

---
+++ encryption.h
@@ -0,0 +1,4 @@
+extern void encrypt_data(unsigned char* unencrypted_data, int size);
+extern void decrypt_data(unsigned char* encrypted_data, int size);
+extern void encrypt_chunked(unsigned char* data, int size);
+extern void decrypt_chunked(unsigned char* data, int size);
